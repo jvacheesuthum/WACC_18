0265e4b3fe4afb42a39d1b46340706eadd39d17b
Kam Chiu <klc114@voxel25.doc.ic.ac.uk>
2015-11-10 14:54:01 +0000

    Symbol Table and dictionary implementations

---
+++ src/SemanticAnalyser/IDENTIFIER.java
@@ -0,0 +1,5 @@
+package SemanticAnalyser;
+
+public class IDENTIFIER {
+
+}
---
+++ src/SemanticAnalyser/SymbolTable.java
@@ -0,0 +1,33 @@
+package SemanticAnalyser;
+
+public class SymbolTable {
+
+	SymbolTable encSymTable;
+	Dictionary dict;
+	
+	SymbolTable(SymbolTable encSymTable) {
+		dict = new Dictionary();
+		this.encSymTable = encSymTable;
+	}
+	
+	public void add(String name, IDENTIFIER obj) {
+		dict.add(name, obj);
+	}
+	
+	public IDENTIFIER lookup(String name) {
+		return dict.get(name);
+	}
+	
+	public IDENTIFIER lookupAll(String name) {
+		SymbolTable st = this;
+		IDENTIFIER obj;
+		while (st != null) {
+			obj = st.lookup(name);
+			if(obj != null) {
+				return obj;
+			}
+			st = encSymTable;
+		}
+		return null;
+	}
+}
