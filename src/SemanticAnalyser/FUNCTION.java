f2c9061e1e7ddc09dc02b0b8cbc65d0d9c7238a3
Ho Law <hcl114@gitlab.doc.ic.ac.uk>
2015-11-16 16:52:43 +0000

    Visit function fix and casting fix

--- src/SemanticAnalyser/FUNCTION.java
+++ src/SemanticAnalyser/FUNCTION.java
@@ -6,12 +6,13 @@ import java.util.List;
 /**
  * Created by vasin on 14/11/2015.
  */
-public class FUNCTION extends IDENTIFIER {
+public class FUNCTION extends VARIABLE {
     public TYPE returntype;
     public List<PARAM> formals = new ArrayList<PARAM>();
     public SymbolTable symtab;
 
     public FUNCTION(TYPE returntype){
+    	super(returntype);
         this.returntype = returntype;
     }
     
--- src/SemanticAnalyser/PARAM.java
+++ src/SemanticAnalyser/PARAM.java
@@ -3,14 +3,9 @@ package SemanticAnalyser;
 /**
  * Created by vasin on 14/11/2015.
  */
-public class PARAM extends IDENTIFIER {
-    TYPE type;
+public class PARAM extends VARIABLE {
 
     public PARAM(TYPE t) {
-        type = t;
-    }
-
-    public TYPE TYPE(){
-        return type;
+    	super(t);
     }
 }
