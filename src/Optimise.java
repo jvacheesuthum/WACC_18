5faefa98b64684320e1c1159b9124ca8c1d7225b
Felix Chiu <kam.chiu14@imperial.ac.uk>
2015-12-16 20:38:06 +0000

    Loop implementation first test

--- src/Optimise.java
+++ src/Optimise.java
@@ -11,16 +11,78 @@ import backEnd.PositionFragment;
 import backEnd.VariableFragment;
 
 public class Optimise {
-
-	public static List<Instruction> loadAndStore(List<Instruction> list, Map<String, Integer> currentStackMap, int stackTotal) {
-		String instrPart; // STR or LDR
-		Integer[] registers = new Integer[15]; //there are 15 reg?
-		Map<Integer, Integer> memory = new HashMap<Integer, Integer>(); //Map<Memory position, value>
-		Integer offset = 0;
-		
-		List<Integer> indexToRemove = new LinkedList<Integer>(); // List for index in instrList to be removed
-		String variable = null; //
+	
+	private static Map<Integer, Integer> memory = new HashMap<Integer, Integer>(); //Map<Memory position, value>
+	private static Integer[] registers = new Integer[15]; //there are 15 reg?
+	private static List<Integer> indexToRemove = new LinkedList<Integer>(); // List for index in instrList to be removed
+	private static Integer offset = 0;
+	private static Map<String, Integer> savedOffset = new HashMap<String, Integer>();
+	private static Integer indexOffset = 0;
+	
+	public static List<Instruction> optimiseInstructions(List<Instruction> list, Map<String, Integer> currentStackMap, int stackTotal) {
+		List<List<Instruction>> sections = new ArrayList<List<Instruction>>();
+		List<Instruction> result = new ArrayList<Instruction>();
+		int fromIndex = 0;
 		for(Instruction l : list) {
+			if(l.toString().contains(":")) {
+				List<Instruction> section = list.subList(fromIndex, list.indexOf(l));
+				sections.add(section);
+				fromIndex = list.indexOf(l);
+			}
+			if(l.toString().contains(".ltorg")) {
+				List<Instruction> section = list.subList(fromIndex, list.indexOf(l) + 1);
+				sections.add(section);
+			}
+		}
+		
+		List<Instruction> main = sections.remove(1);
+		sections.add(main);
+		for(int i = sections.size() - 1; i > 0; i--) {
+			main = sections.get(i);
+//			System.out.println("savedOffset: " + savedOffset);
+			main = loadAndStore(main, currentStackMap, stackTotal);
+//			System.out.println("IndexToRemove: " + indexToRemove);
+//			indexOffset += main.size() - 1;
+//			memory = new HashMap<Integer, Integer>();
+//			offset = 0;
+			result.addAll(main);
+		}
+//		ListIterator<Integer> it = indexToRemove.listIterator(indexToRemove.size());
+//		Integer index;
+//		while(it.hasPrevious()) {
+//			index = it.previous();
+//			list.remove((int)index);
+//			it.remove();
+//		}
+		return result;
+	}
+	public static List<Instruction> loadAndStore(List<Instruction> list, Map<String, Integer> currentStackMap, int stackTotal) {
+		List<Instruction> result = new ArrayList<Instruction>();
+		result.addAll(list);
+		String instrPart; // STR or LDR
+		String variable = null; //
+		Integer tempOffset = 0;
+		String startLabel = list.get(0).toString();
+		startLabel = startLabel.substring(0, startLabel.indexOf(":"));
+		System.out.println("StartLabel: " + startLabel);
+		if(startLabel.contains("L")) {
+			System.out.println("SavedOffset: " + savedOffset);
+			System.out.println("SupposedLabel: " + savedOffset.get("LW0"));
+			System.out.println("ActualLabel: " + startLabel);
+			if(savedOffset.containsKey(startLabel)) {
+				tempOffset = offset;
+				offset = savedOffset.get(startLabel);
+				System.out.println("OFFSET BEFORE LIST TRAVERSAL: " + offset);
+			}
+		}
+		System.out.println("OFFSET START: " + offset);
+		for(Instruction l : list) {
+			System.out.print("Registers: [");
+			for(int i = 0; i < 15; i++) {
+				System.out.print(registers[i] + ", ");
+			}
+			System.out.print("]\n");
+			System.out.println("Memory: " + memory);
 			if (l.toDeclare()) {
 				stackTotal = l.allocateStackPos(stackTotal, currentStackMap);
 			} // needed whether or not is is to be optimised
@@ -37,10 +99,14 @@ public class Optimise {
 				System.out.println("STR VARIABLE: " + variable);
 				System.out.println("STR VARIABLE AFTER LOOKUP: " + currentStackMap.get(variable));
 			}
-//			if(first.contains(":")) {
-////				afterStr = false;
-//				continue;
-//			} //if label, continue on next instruction
+			if(first.charAt(0) == 'B' || first.contains("BEQ")) {
+				System.out.println("BRANCH STATEMENT");
+				String label = first.substring(first.indexOf('L'));
+				label = label.substring(0, label.indexOf("\n"));
+				System.out.println("LABEL: " + label);
+				savedOffset.put(label, offset);
+				System.out.println("SavedOFFSET: " + savedOffset);
+			}
 			if(first.contains("SUB sp, sp")) {
 				if(l.getVariables() != null) {
 					variable = l.getVariables().get(0).getVariable();
@@ -58,9 +124,11 @@ public class Optimise {
 					variable = l.getVariables().get(0).getVariable();
 					offset -= currentStackMap.get(variable);
 				} else {
-					offset -= Integer.parseInt(first.substring(13, 14));
+					offset -= Integer.parseInt(first.substring(13, first.length()-1));
 				}
-				memory = clear(prevOffset, offset, memory);
+				System.out.println("PREVOFFSET BEFORE CLEAR: " + prevOffset);
+				System.out.println("OFFSET BEFORE CLEAR: " + offset);
+//				memory = clear(prevOffset, offset, memory);
 				System.out.println("OFFSET: " + offset);
 				System.out.println("=============END==============");
 				continue;
@@ -69,7 +137,7 @@ public class Optimise {
 				currRegCount = Integer.parseInt(first.substring(6, 7));
 				Integer lhsReg = Integer.parseInt(first.substring(10, 11));
 				Integer rhsReg = Integer.parseInt(first.substring(14, 15));
-				if(registers[rhsReg] == null) System.out.println("NULLLLLLLLLLLLL");
+				if(registers[currRegCount] == null) System.out.println("NULLLLLLLLLLLLL");
 				registers[currRegCount] = registers[lhsReg] + registers[rhsReg];
 				continue;
 			}
@@ -119,7 +187,8 @@ public class Optimise {
 					continue;
 				}
 				if(memory.get(index).equals(registers[currRegCount])) {
-					indexToRemove.add(list.indexOf(l));
+//					indexToRemove.add(list.indexOf(l) + indexOffset);
+					result.remove(l);
 				} else {
 					memory.put(index, registers[currRegCount]);
 					System.out.println("REGISTER R: " + registers[currRegCount]);
@@ -144,31 +213,25 @@ public class Optimise {
 				System.out.println("VALUE BEFORE: " + value);
 				System.out.println("OFFSET: " + offset);
 				if(first.charAt(9) == 's') {
-					continue;
-//					if(value == 0) continue;
-//					value = memory.get(offset - value);
+					if(value == 0) continue;
+					value = memory.get(offset - value);
 				}
 				System.out.println("REG VALUE: " + registers[currRegCount]);
 				System.out.println("VALUE AFTER: " + value);
 				if(registers[currRegCount] == value) {
-					indexToRemove.add(list.indexOf(l));
+//					indexToRemove.add(list.indexOf(l) + indexOffset);
+					result.remove(l);
 				} else {
 					registers[currRegCount] = value;
 				}
 			}
-//			count++;
 			System.out.println("=============END==============");
 		}
-		List<Instruction> tempList = new ArrayList<Instruction>();
-		tempList.addAll(list);
-		ListIterator<Integer> it = indexToRemove.listIterator(indexToRemove.size());
-		Integer index;
-		while(it.hasPrevious()) {
-			index = it.previous();
-			tempList.remove((int)index);
-			it.remove();
+		if(savedOffset.containsKey(list.get(0).toString())) {
+			offset = tempOffset;
 		}
-		return tempList;
+		return result;
+		
 	}
 	
 	private static Integer findOffset(Instruction l, int pos) {
