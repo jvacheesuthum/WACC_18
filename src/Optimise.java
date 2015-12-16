574a46240e9348d62cb81c1917ab73de30543886
Felix Chiu <kam.chiu14@imperial.ac.uk>
2015-12-16 20:50:43 +0000

    remove continue in ldr check when value == 0

--- src/Optimise.java
+++ src/Optimise.java
@@ -213,7 +213,7 @@ public class Optimise {
 				System.out.println("VALUE BEFORE: " + value);
 				System.out.println("OFFSET: " + offset);
 				if(first.charAt(9) == 's') {
-					if(value == 0) continue;
+//					if(value == 0) continue;
 					value = memory.get(offset - value);
 				}
 				System.out.println("REG VALUE: " + registers[currRegCount]);
