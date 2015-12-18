1e7ea34763fdb597a2282aa3ffe7e8ac9a1b5d11
Derek <hcl114@gitlab.doc.ic.ac.uk>
2015-12-18 22:44:22 +0800

    main changed to enable extensions

--- src/Main.java
+++ src/Main.java
@@ -54,30 +54,43 @@ public class Main {
 		    parser.addErrorListener(SyntaxErrorListener.INSTANCE);
 
 		    
-		    
-		    ParseTree tree = parser.program(); // begin parsing at program rule
-
-		   
-		    // build and run my custom visitor
-		    
 		    String filename = args[0].substring(args[0].lastIndexOf('/')+1, args[0].length()-5);
 		    
-		    System.out.println("====Optimising====");
-		    tree = new VariableVisitor().visit(tree);
-		    System.out.println("====Finished First Pass====");
+		    ParseTree tree = parser.program(); // begin parsing at program rule
+		    // build and run my custom visitor
 		    
+		  	MyWaccVisitor visitor = new MyWaccVisitor(filename + ".s");
 		    
-		    System.out.println("====Optimising 2 ====");
-	    	tree = new BinopTreeReorder().visit(tree).getCtx();
-	    	System.out.println("====Finished Second Pass====");
-	    	
-		    
+		    //check optimisations
+		    if (args.length > 1) {
+		    	switch (args[1]) {
+		    	case "-cEval": visitor = new OptimisedWaccVisitor(filename + ".s"); break;
+		    	case "-cProp": detectConstantVars(tree);
+		    					visitor = new OptimisedWaccVisitor(filename + ".s"); break;
+		    	case "-cPBtr": detectConstantVars(tree); btr(tree);
+		    					visitor = new OptimisedWaccVisitor(filename + ".s"); break;
+		    	case "-CFA": visitor = new WaccVisitorWithCFA(filename + ".s"); break;
+		    	// add here!
+		    	case "-IE": break;
+		    	}
+		    }
+
 		    System.out.println("====");
-		    MyWaccVisitor visitor = new OptimisedWaccVisitor(filename + ".s");
-		  	//MyWaccVisitor visitor = new MyWaccVisitor(filename + ".s");
 		    visitor.visit(tree);
 		    System.out.println("====");
 		    System.exit(0);
 		  }
+
+	private static void btr(ParseTree tree) {
+	    System.out.println("====Optimising 2 ====");
+    	tree = new BinopTreeReorder().visit(tree).getCtx();
+    	System.out.println("====Finished Second Pass====");
+	}
+
+	private static void detectConstantVars(ParseTree tree) {
+	    System.out.println("====Optimising====");
+	    tree = new VariableVisitor().visit(tree);
+	    System.out.println("====Finished First Pass====");
+	}
 	
 }
